4598a2c9ca3d3d934a8c8f6dabd0315da714358b
Garrett Smith <ggasmithh@users.noreply.github.com>
2019-10-24 18:35:02 -0500

    Working ACKs

--- client.cpp
+++ client.cpp
@@ -26,8 +26,8 @@ using namespace std;
 
 int main(int, char *argv[]) {
     struct hostent *s;
-    struct sockaddr_in server;
-    struct sockaddr_in response_server;
+    struct sockaddr_in send_server;
+    struct sockaddr_in recv_server;
     char file_buffer[MAX_BUFFER_SIZE];
     char send_buffer[MAX_BUFFER_SIZE];
     char recv_buffer[MAX_BUFFER_SIZE];
@@ -37,6 +37,9 @@ int main(int, char *argv[]) {
     int send_port;
     int recv_port;
     const char *filename = argv[4];
+      
+    char null_data[32];
+    memset(null_data, 0, 32);
     
     istringstream(argv[2]) >> send_port;
     istringstream(argv[3]) >> recv_port;
@@ -44,17 +47,17 @@ int main(int, char *argv[]) {
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
     s = gethostbyname(emulator_host);
-    memset((char *) &server, 0, sizeof(server));
-    server.sin_family = AF_INET;
-    server.sin_port = htons(send_port);
-    bcopy((char *)s->h_addr, (char *)&server.sin_addr.s_addr, s->h_length);
+    memset((char *) &send_server, 0, sizeof(send_server));
+    send_server.sin_family = AF_INET;
+    send_server.sin_port = htons(send_port);
+    bcopy((char *)s->h_addr, (char *)&send_server.sin_addr.s_addr, s->h_length);
 
-    memset((char *) &response_server, 0, sizeof(response_server));
-    response_server.sin_family = AF_INET;
-    response_server.sin_port = htons(recv_port);
-    response_server.sin_addr.s_addr = htonl(INADDR_ANY);
-    bind(sockfd, (struct sockaddr *)&response_server, sizeof(response_server));
-    socklen_t rslen = sizeof(response_server);
+    memset((char *) &recv_server, 0, sizeof(recv_server));
+    recv_server.sin_family = AF_INET;
+    recv_server.sin_port = htons(recv_port);
+    recv_server.sin_addr.s_addr = htonl(INADDR_ANY);
+    bind(sockfd, (struct sockaddr *)&recv_server, sizeof(recv_server));
+    socklen_t rslen = sizeof(recv_server);
 
     ifstream infile(filename);
     stringstream file_contents_stream;
@@ -74,16 +77,25 @@ int main(int, char *argv[]) {
         pkt.printContents();
         pkt.serialize(send_buffer);
 
-        sendto(sockfd, send_buffer, sizeof(send_buffer), 0, (struct sockaddr *)&server, sizeof(server));
+        sendto(sockfd, send_buffer, sizeof(send_buffer), 0, (struct sockaddr *)&send_server, sizeof(send_server));
+        recvfrom(sockfd, recv_buffer, sizeof(recv_buffer), 0, (struct sockaddr *)&recv_server, &rslen);
+
+        packet ack(0, 0, 30, null_data);
+        ack.deserialize(recv_buffer);
+
+        if (ack.getType() == 0) {
+            cout << "ACK RECEIVED" << endl;
+        } else {
+            cout << "INVALID ACK" << endl;
+            return 1;
+        }
     }
 
     packet end_pkt(3, 0, 0, NULL);
     end_pkt.serialize(send_buffer);
-    sendto(sockfd, send_buffer, sizeof(send_buffer), 0, (struct sockaddr *)&server, sizeof(server));
+    sendto(sockfd, send_buffer, sizeof(send_buffer), 0, (struct sockaddr *)&send_server, sizeof(send_server));
 
-    recvfrom(sockfd, recv_buffer, sizeof(recv_buffer), 0, (struct sockaddr *)&response_server, &rslen);
-    char null_data[32];
-    memset(null_data, 0, 32);
+    recvfrom(sockfd, recv_buffer, sizeof(recv_buffer), 0, (struct sockaddr *)&recv_server, &rslen);
 
     packet pkt(0, 0, 30, null_data);
     pkt.deserialize(recv_buffer);
@@ -92,6 +104,7 @@ int main(int, char *argv[]) {
         close(sockfd);
         return 0;
     } else {
+        cout << "INVALID ENDPACKET" << endl;
         return 1;
     }
 
--- server.cpp
+++ server.cpp
@@ -25,8 +25,9 @@
 using namespace std;
 
 int main(int, char* argv[]) {
-    struct sockaddr_in server;
-    struct sockaddr_in client;
+    // There *has* to be a better naming convention for these two sockaddr_in structs
+    struct sockaddr_in recv_server;
+    struct sockaddr_in send_server;
     char recv_buffer[MAX_BUFFER_SIZE];
     char send_buffer[MAX_BUFFER_SIZE];
     char file_buffer[MAX_BUFFER_SIZE];
@@ -37,37 +38,43 @@ int main(int, char* argv[]) {
     int recv_port;
     int send_port;
     const char *filename = argv[4];
+ 
+    char null_data[32];
+    memset(null_data, 0, 32);
     
     istringstream(argv[2]) >> recv_port;
     istringstream(argv[3]) >> send_port;
 
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
  
-    memset((char *)&server, 0, sizeof(server));
-    server.sin_family = AF_INET;
-    server.sin_port = htons(recv_port);
-    server.sin_addr.s_addr = htonl(INADDR_ANY);
-    bind(sockfd, (struct sockaddr *)&server, sizeof(server));
-    
-    socklen_t clen = sizeof(client);
+    memset((char *)&recv_server, 0, sizeof(recv_server));
+    recv_server.sin_family = AF_INET;
+    recv_server.sin_port = htons(recv_port);
+    recv_server.sin_addr.s_addr = htonl(INADDR_ANY);
+    bind(sockfd, (struct sockaddr *)&recv_server, sizeof(recv_server));
+    socklen_t rslen = sizeof(recv_server);
+
+    memset((char *)&send_server, 0, sizeof(send_server));
+    send_server.sin_family = AF_INET;
+    send_server.sin_port = htons(send_port);
+    send_server.sin_addr.s_addr = htonl(INADDR_ANY); 
 
     while (1) {
         memset((char *)&recv_buffer, 0, sizeof(recv_buffer));
-        recvfrom(sockfd, recv_buffer, sizeof(recv_buffer), 0, (struct sockaddr *)&client, &clen);
-    
-        char null_data[32];
-        memset(null_data, 0, 32);
+        recvfrom(sockfd, recv_buffer, sizeof(recv_buffer), 0, (struct sockaddr *)&recv_server, &rslen);
 
         packet pkt(0, 0, 30, null_data);
         pkt.deserialize(recv_buffer);
 
-        if(pkt.getType() == 3) {
+        if (pkt.getType() == 3) {
             packet end_pkt(2, 0, 0, NULL);
             end_pkt.serialize(send_buffer);
-            server.sin_port = htons(send_port);
-            sendto(sockfd, send_buffer, sizeof(send_buffer), 0, (struct sockaddr *)&server, sizeof(server));
+            sendto(sockfd, send_buffer, sizeof(send_buffer), 0, (struct sockaddr *)&send_server, sizeof(send_server));
             break;
         } else {
+            packet ack(0, 0, 0, NULL);
+            ack.serialize(send_buffer);
+            sendto(sockfd, send_buffer, sizeof(send_buffer), 0, (struct sockaddr *)&send_server, sizeof(send_server));
             complete_data += pkt.getData();
         }
     } 
